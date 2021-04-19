4420de5f0d1cfed7a9fa61c846b648a2e85053c4
Mahmoudfad <you@example.commahmoudfadhel2@gmail.com>
2021-04-19 15:44:28 +0100

    mm

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,49 @@
-// var express = require('express');
-// var router = express.Router();
-// let csvToJson = require('convert-csv-to-json');
-// var langdetect = require('langdetect');
-
-// console.log(langdetect.detect('Questo a che ora comincia '));
-
-// let fileInputName = 'Classeur1.csv'; 
-// let fileOutputName = 'myOutputFile.json';
-
-// csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
-
 var express = require('express');
 var router = express.Router();
-const csvtojson = require ('csvtojson')
+const csv = require('csv-parser')
 const fs = require ('fs')
 const path = require ('path');
 const { json } = require('express');
 const { Schema } = require('mongoose');
 var listSchema = require ('../schema/list')
+const translate = require('@vitalets/google-translate-api');
+const list = {}
+var result = []
 
+router.post('/add',  (req, res)=>{
+  fs.createReadStream('Classeur1.csv')
+  .pipe(csv())
+  .on('data', (data) => result.push(data))
+  .on('end', () => {
+    console.log(result);
+   
+  });
+result.forEach(element => {
+ list = new listSchema({
+    nom: element.nom,
+    prenom: element.prenom,
+    age: element.age,
+    email: element.email
+   })
+   list.save().then(x=>{
+    res.json({message:"sent"})})
 
-const csvfilepath = path.resolve('./csv', 'Classeur1.csv');
-
-csvtojson()
-.fromFile(csvfilepath)
-.then((json)=>{
-  console.log(json);
-
+}).catch(err=>{res.send(err)})
 })
+
+
+
+
+
+
+
+translate('prenom', {to: 'en'}).then(res => {
+    console.log(res.text);
+    console.log(res.from.language.iso);
+})
+// .catch(err => {
+//     console.error(err);
+// });
 
 
 
