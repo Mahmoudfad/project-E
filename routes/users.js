09ee931f964cae3feb5fed49ea374f536d2b2520
Mahmoudfad <you@example.commahmoudfadhel2@gmail.com>
2021-04-21 14:39:18 +0100

    aa

--- routes/users.js
+++ routes/users.js
@@ -6,44 +6,42 @@ const path = require ('path');
 const { json } = require('express');
 const { Schema } = require('mongoose');
 var listSchema = require ('../schema/list')
-const translate = require('@vitalets/google-translate-api');
-const list = {}
 var result = []
 
-router.post('/add',  (req, res)=>{
-  fs.createReadStream('Classeur1.csv')
-  .pipe(csv())
-  .on('data', (data) => result.push(data))
-  .on('end', () => {
-    console.log(result);
-   
-  });
+router.post('/add',  (req, res , next)=>{
+fs.createReadStream('routes/Classeur1.csv')
+.pipe(csv())
+.on('data', (data) => result.push(data))
+.on('end', () => {
+  console.log(result);
+ 
+});
+ 
 result.forEach(element => {
- list = new listSchema({
+  const list = new listSchema({
     nom: element.nom,
     prenom: element.prenom,
     age: element.age,
     email: element.email
    })
    list.save().then(x=>{
-    res.json({message:"sent"})})
+    res.json(x)
+  }).catch(err=>res.send(err))
 
-}).catch(err=>{res.send(err)})
-})
-
-
-
+  })
+});
 
 
 
 
-translate('prenom', {to: 'en'}).then(res => {
-    console.log(res.text);
-    console.log(res.from.language.iso);
-})
-// .catch(err => {
-//     console.error(err);
-// });
+
+
+
+// translate('prenom', {to: 'en'}).then(res => {
+//     console.log(res.text);
+//     console.log(res.from.language.iso);
+// })
+
 
 
 
